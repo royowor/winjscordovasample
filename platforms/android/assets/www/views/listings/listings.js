a88789b4b13d0e08c3b6fe6daaec4d74c6e0bef1
Penguin Apps Ltd <agritradersoftware@gmail.com>
2015-03-03 14:11:58 +0300

    winjs appbar and navigation

---
+++ platforms/android/assets/www/views/listings/listings.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    "use strict";
+
+    var appBar;
+    window.appBarCommandsToShow = undefined;
+
+    WinJS.UI.Pages.define("./views/listings/listings.html", {
+
+        ready: function (element, options) {
+
+            appBar = document.getElementById("appBar").winControl;
+            appBar.getCommandById('cmd1').addEventListener("click", cmd1, false);
+
+            appBar.getCommandById('cmd2').addEventListener("click", cmd2, false);
+
+            this.appBarShowOnlyCommands(undefined);
+
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            // / <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        },
+
+
+        // appBar properties
+        btnPrevious: function (args) {
+            WinJS.Navigation.navigate('./views/listings/listings.html', args);
+        },
+
+        appBarState: function () {
+            appBar.closedDisplayMode = 'minimal';
+            appBar.hide()
+            appBar.show();
+
+        },
+
+        appBarShowOnlyCommands: function (cmds) {
+
+            var defaultButtons = [cmdCrawl, cmdDownload];
+
+            appBar.closedDisplayMode = 'minimal';
+            appBar.sticky = false;
+            appBar.show();
+
+            if (cmds !== undefined && cmds.length > 0) {
+                appBar.showOnlyCommands(cmds);
+
+            } else {
+                appBar.showOnlyCommands(defaultButtons);
+            }
+        }
+    });
+
+
+
+    function cmd1() {
+        console.log("cmd1 clicked");
+    }
+
+    function cmd2() {
+        console.log("cmd2 clicked");
+    }
+
+})();
---
+++ www/js/index.js
@@ -0,0 +1,48 @@
+﻿(function(window) {
+	"use strict";
+
+	var app = WinJS.Application;
+	var nav = WinJS.Navigation;
+	var sched = WinJS.Utilities.Scheduler;
+	var ui = WinJS.UI;
+	var _window = window;
+
+	
+
+	document.addEventListener("deviceready", onReady, false);
+	document.addEventListener("resume", onResume, false);
+
+	function onReady() {
+		// Handle the deviceready event.
+		initialize();
+		document.addEventListener("offline", _window.Offline, false);
+		document.addEventListener("offline", _window.Online, false);
+		
+		
+	}
+
+	function onResume() {
+		// Handle the resume event
+	}
+
+	function initialize() {
+
+		nav.history = app.sessionState.history || {};
+		nav.history.current.initialPlaceholder = true;
+
+		// Optimize the load of the application and while the splash screen is
+		// shown, execute high priority scheduled work.
+		ui.disableAnimations();
+		var p = ui.processAll().then(function() {
+			return nav.navigate(nav.location || Application.navigator.home, nav.state);
+		}).then(function() {
+			return sched.requestDrain(sched.Priority.aboveNormal + 1);
+		}).then(function() {
+			ui.enableAnimations();
+		});
+
+		
+	}
+
+	
+})(window);
